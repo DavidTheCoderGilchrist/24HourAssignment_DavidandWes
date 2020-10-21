888694873b1e412d5ea2b4ab8257bd1fb67a8665
Gilly 🐧 <dagilchr@gmail.com>
2020-10-21 16:54:13 -0400

    Test Commit One

---
+++ 24HourAssignment_DavidandWes/Models/TestClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace _24HourAssignment_DavidandWes.Models
+{
+    public class TestClass
+    {
+        public static ApplicationDbContext Test()
+        {
+            return new ApplicationDbContext();
+        }
+
+    }
+}
